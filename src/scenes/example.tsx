380413031129006173b4a905c0f09faded43422d
Thierry Berger <contact@thierryberger.com>
2023-10-18 23:49:01 +0200

    test spawner

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,5 +1,5 @@
 import { makeScene2D, Circle, Layout, Rect, Txt, Img } from '@motion-canvas/2d';
-import { all, createRef, createSignal, easeInBack, easeInBounce, easeInExpo, easeInQuad, range } from '@motion-canvas/core';
+import { all, createRef, createSignal, easeInBack, easeInBounce, easeInExpo, easeInQuad, range, waitFor } from '@motion-canvas/core';
 import background from '../assets/message_background.png';
 
 export default makeScene2D(function* (view) {
@@ -10,7 +10,7 @@ export default makeScene2D(function* (view) {
       <Img src={background} />
       <Txt ref={myTxt}>Hello</Txt>
       <Layout spawner={() =>
-        range(my_count()).map((i) => <Txt y={200 + i * 42} width={32} height={32} fill={'white'}>{i}</Txt>)}
+        range(my_count() + 1).map((i) => <Txt y={200 + i * 42} width={32} height={32} fill={'white'}>{i}</Txt>)}
       />
 
     </>,
@@ -22,8 +22,10 @@ export default makeScene2D(function* (view) {
   );
 
   yield* all(
-    my_count(1, 1).to(2, 1).to(3, 1),
+    my_count(1, 1), my_count(2, 1).to(3, 1),
   );
+
+  yield* waitFor(2)
 });
 function count(): number {
   throw new Error('Function not implemented.');
