e38e87248d92a1ff4914c58f12715f98b6a5b090
Thierry Berger <contact@thierryberger.com>
2023-11-01 14:47:25 +0100

    bubble for typing text

--- src/scenarios/scenario_bff.tsx
+++ src/scenarios/scenario_bff.tsx
@@ -2,46 +2,46 @@ export class data {
     public static all_data = [{
         text: "Hey, remember our school days?",
         from_me: true,
-        transition_duration: 5,
+        time: 2,
     }, {
         text: "Oh, totally! Those were the best, so many good times.",
         from_me: false,
-        transition_duration: 5.5,
+        time: 5,
     }, {
         text: "Lunch breaks chatting, boring classes we survived together, and the promises to be BFFs forever.",
         from_me: true,
-        transition_duration: 5,
+        time: 10,
     }, {
         text: "Looking at old school pics makes me tear up, but in a good way.",
         from_me: false,
-        transition_duration: 5.5,
+        time: 15,
     }, {
         text: "I'm tearing up right now too. Time flew by, but our friendship never changed.",
         from_me: true,
-        transition_duration: 6,
+        time: 20,
     }, {
         text: "Nothing's changed there. You're my ride-or-die. I'm so lucky to have you.",
         from_me: false,
-        transition_duration: 5,
+        time: 25,
     }, {
         text: "Same here, you're irreplaceable. Can't imagine life without you.",
         from_me: true,
-        transition_duration: 5,
+        time: 30,
     }, {
         text: "We gotta meet up soon, relive a piece of our past.",
         from_me: true,
-        transition_duration: 6
+        time: 35,
     }, {
         text: "Totally, I miss it so much. Love you more than words can say.",
         from_me: false,
-        transition_duration: 5,
+        time: 40,
     }, {
         text: "❤️❤️❤️❤️❤️❤️",
         from_me: true,
-        transition_duration: 5,
+        time: 45,
     }, {
         text: "❤️❤️❤️❤️❤️❤️",
         from_me: false,
-        transition_duration: 2,
+        time: 50,
     }];
 }
--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -13,7 +13,8 @@ export default makeScene2D(function* (view) {
   const my_messages = createSignal([]);
   // <Video loop={true} ref={videoRef} src={hypno} height="100%" width="100%" />
 
-  const indicator_other_typing_signal = createSignal(1);
+  const indicator_other_typing_signal = createSignal(0);
+  const indicator_me_typing_signal = createSignal(0);
   view.add(
     <Layout>
       <Img src={background} />
@@ -28,9 +29,15 @@ export default makeScene2D(function* (view) {
           return messages;
         }
         } />
-        <Rect scale={() => [indicator_other_typing_signal(), indicator_other_typing_signal()]} alignSelf="start" margin={[0, 255, 0, 50]} padding={[45, 10, 45, 10]} fill={'#444444'} radius={90}>
-          <Txt textWrap={true} position={[0, 0]} alignSelf="center" margin={[0, 50, 0, 50]} fontSize={80} fill={'#ffffff'}>...</Txt >
+        <Rect alignSelf="stretch" radius={90}>
+          <Rect scale={() => [indicator_other_typing_signal(), indicator_other_typing_signal()]} alignSelf="start" margin={[0, 255, 0, 50]} padding={[45, 10, 45, 10]} fill={'#444444'} radius={90}>
+            <Txt textWrap={true} position={[0, 0]} alignSelf="center" margin={[0, 50, 0, 50]} fontSize={80} fill={'#ffffff'}>...</Txt >
+          </Rect>
+          <Rect scale={() => [indicator_me_typing_signal(), indicator_me_typing_signal()]} alignSelf="end" margin={[0, -50, 0, 355]} padding={[45, 10, 45, 10]} fill={'#3070ff'} radius={90} offsetX={1}>
+            <Txt textWrap={true} position={[0, 0]} alignSelf="center" margin={[0, 50, 0, 50]} fontSize={80} fill={'#ffffff'}>...</Txt >
+          </Rect>
         </Rect>
+
       </Layout>
       <Layout position={[0, 150]}>
         <Rect layout width="100%" offset={[0, 1]} position={[0, -1000]} height={600} fill="#333333" />
@@ -39,20 +46,37 @@ export default makeScene2D(function* (view) {
           <Txt alignSelf="center" fill="#ffffff">❤️</Txt>
         </Layout>
       </Layout>
-    </Layout>,
+    </Layout >,
   );
   //videoRef().play();
   var states = [];
   for (var i = 0; i < all_data.length; i++) {
     states.push(all_data.slice(0, i + 1));
   }
-
+  var indicator_other_typing_signal_scenario = indicator_other_typing_signal(0, 0);
+  var indicator_me_typing_signal_scenario = indicator_me_typing_signal(0, 0);
   var scenario = my_messages([], 0);
-  for (var i = 0; i < states.length; i++) {
-    scenario = scenario.to(states[i], all_data[i].transition_duration);
+  var current_time = 0;
+  var typing_duration = 2;
+  for (var i = 0; i < all_data.length; i++) {
+    var messages = all_data.slice(0, i + 1);
+    var data_current_message = all_data[i];
+    var additional_time = data_current_message.time - current_time;
+    scenario = scenario.wait(additional_time).to(messages, 0);
+    indicator_other_typing_signal_scenario = indicator_other_typing_signal_scenario.wait(additional_time - typing_duration);
+    indicator_me_typing_signal_scenario = indicator_me_typing_signal_scenario.wait(additional_time - typing_duration);
+    if (data_current_message.from_me == false) {
+      indicator_other_typing_signal_scenario = indicator_other_typing_signal_scenario.to(1, 0.5).wait(typing_duration - 1).to(0, 0.5);
+      indicator_me_typing_signal_scenario = indicator_me_typing_signal_scenario.wait(typing_duration)
+    } else {
+      indicator_other_typing_signal_scenario = indicator_other_typing_signal_scenario.wait(typing_duration)
+      indicator_me_typing_signal_scenario = indicator_me_typing_signal_scenario.to(1, 0.5).wait(typing_duration - 1).to(0, 0.5);
+    }
+    current_time += additional_time;
   }
   yield* all(
-    indicator_other_typing_signal(0, 0.5).to(1, 0.5).to(0, 0.5).to(1, 0.5).to(0, 0.5).to(1, 0.5).to(0, 0.5),
+    indicator_other_typing_signal_scenario,
+    indicator_me_typing_signal_scenario,
     scenario,
   );
 
